65f467672d36c1e3f1d1062caec537f4a4199576
JayaRose <Jayarose8@gmail.com>
2024-07-25 12:08:39 +1200

    Playing Variables
    Got variables up and running

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,7 +1,9 @@
 //your parameter variables go here!
 let rect_width  = 20;
 let rect_height = 20;
-
+var color1 = "#679624"
+var endAngle = 350
+var startAngle = 75
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -15,9 +17,20 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(44, 154, 209); //light honeydew green colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+  
+ 
+  fill(color1)
+  arc(150, 50, 80, 40, startAngle, endAngle, PIE);
+
+  fill(39, 92, 13)
+  ellipse(100, 100, 80, 40, 6);
+  
+  fill(49, 140, 4)
+  circle(50, 50, 65);
+
+
 }
