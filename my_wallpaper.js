538501df50fef18c4231e6dbc09a2783e8b31a64
JayaRose <Jayarose8@gmail.com>
2024-07-25 23:50:08 +1200

    random size variable
    I added in a random size variable using let  size

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -20,8 +20,16 @@ var DarkGreenStartAngle = 0
 var SwampGreenEndAngle = 320
 var SwampGreenStartAngle = 0
 
+var LightSwampGreenStartingAngle = 45
+var LightSwampGreenEndAngle = 0
+
+var DarkSwampGreenStartAngle = 45
+var DarkSwampGreenEndAngle =  0
+
+var size = random(25, 60);
+
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
@@ -32,20 +40,29 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(44, 154, 209); //light honeydew green colour
+  background(27, 83, 105); // Blue
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   
+  let x = random(50, 150);
+    let y = random(50, 150);
+    let size = random(25, 60); // Random size between 40 and 80
  
   fill(color1)
-  arc(150, 50, 80, 40, LightGreenStartAngle, LightGreenEndAngle, PIE);
+  arc(150, 50, size, size, LightGreenStartAngle, LightGreenEndAngle, PIE);
 
   fill(color2)
-  arc(100, 100, 80, 40, DarkGreenStartAngle, DarkGreenEndAngle, PIE);
+  arc(100, 100, size, size, DarkGreenStartAngle, DarkGreenEndAngle, PIE);
   
   fill(color3)
-  arc(50, 50, 65, 65, SwampGreenStartAngle, SwampGreenEndAngle, PIE);
+  arc(50, 50, size, size, SwampGreenStartAngle, SwampGreenEndAngle, PIE);
 
+  fill(138, 184, 2)
+  arc(50, 170, size, size, LightSwampGreenStartingAngle, LightSwampGreenEndAngle, PIE);
 
+  fill(39, 66, 5)
+  arc(145, 145, size, size, DarkSwampGreenStartAngle, DarkSwampGreenEndAngle, PIE);
+
+ 
 }
